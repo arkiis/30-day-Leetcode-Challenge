49ff39b0190b9d883e39a62634da08e2e74217e6
Your Name <william.fleckenstein@gmail.com>
2020-05-04 11:38:59 -0500

    added comments

--- WEEK-1/Day-3.js
+++ WEEK-1/Day-3.js
@@ -1,11 +1,13 @@
 //Searching
 var canConstruct = function (ransomNote, magazine) {
-  let ransom = [...ransomNote];
-  let stack = [];
+  let ransom = [...ransomNote]; //splitting string into an array
+  let stack = []; //pushing matched letters
 
   for (let char of magazine) {
+    //looping thru magazine
     if (ransom.includes(char)) {
-      let pop = ransom.splice(ransom.indexOf(char), 1);
+      //searching for a match
+      let pop = ransom.splice(ransom.indexOf(char), 1); //store matched value in a var
 
       stack.push(pop);
     }
