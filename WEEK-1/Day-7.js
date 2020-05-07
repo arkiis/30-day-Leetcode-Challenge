d2805d8c678cd1d0a9a5f786ee04789a820ecca0
Your Name <william.fleckenstein@gmail.com>
2020-05-07 16:20:41 -0500

    Day-7 completed Day-7

---
+++ WEEK-1/Day-7.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} x
+ * @param {number} y
+ * @return {boolean}
+ */
+var isCousins = function (root, x, y) {
+  //BFS -- same level order traversal using queue
+  let map = {},
+    q = [];
+  q.push(root);
+  map[root.val] = { parent: null, level: 0 };
+
+  while (q.length) {
+    let node = q.shift();
+
+    if (node.left) {
+      map[node.left.val] = { parent: node.val, level: map[node.val].level + 1 };
+      q.push(node.left);
+    }
+
+    if (node.right) {
+      map[node.right.val] = {
+        parent: node.val,
+        level: map[node.val].level + 1,
+      };
+      q.push(node.right);
+    }
+  }
+
+  return map[x].parent !== map[y].parent && map[x].level === map[y].level;
+};
